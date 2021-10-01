2c1e64d971a7451033d2e0a1389692096818dc43
Manuel Boy <info@manuelboy.de>
2021-10-01 11:21:35 +0200

    Make tests pass

--- test/phrase/ota/rails_test.rb
+++ test/phrase/ota/rails_test.rb
@@ -4,8 +4,4 @@ class Phrase::Ota::RailsTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Phrase::Ota::Rails::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
