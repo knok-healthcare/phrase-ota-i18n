82eaac8535dec20b8e755c153d7edc840e2fb8a7
Sönke Behrendt <soenke.behrendt@gmail.com>
2023-04-12 16:02:33 +0200

    Add configuration spec

--- spec/i18n/backend/phrase_ota_spec.rb
+++ spec/i18n/backend/phrase_ota_spec.rb
@@ -1,9 +1,13 @@
 describe I18n::Backend::PhraseOta do
   context "#available_locales" do
+    I18n::Backend::PhraseOta.configure do |config|
+      config.available_locales = [:en, :de]
+    end
+
     backend = I18n::Backend::PhraseOta.new
 
     it do
-      expect(backend.available_locales).to(eq([]))
+      expect(backend.available_locales).to(eq([:en, :de]))
     end
   end
 end
