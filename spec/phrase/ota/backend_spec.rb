3d234072a8631577280fee280b93abb3e1cd2e2b
Sönke Behrendt <soenke.behrendt@gmail.com>
2023-09-01 15:35:27 +0200

    fix: store current_version per locale

--- spec/phrase/ota/backend_spec.rb
+++ spec/phrase/ota/backend_spec.rb
@@ -23,6 +23,7 @@ describe Phrase::Ota::Backend do
       config.available_locales = []
     end
     I18n.backend = @previous_backend
+    I18n.config.clear_available_locales_set
   end
 
   context "#available_locales" do
@@ -42,7 +43,7 @@ describe Phrase::Ota::Backend do
 
       context "translations should be updated" do
         before do
-          stub_request(:get, url_en).to_return(status: 200, body: body_en, headers: {})
+          stub_request(:get, url_en).to_return(status: 200, body: body_en)
         end
 
         it do
@@ -56,8 +57,8 @@ describe Phrase::Ota::Backend do
         let(:body_en_2) { {"en" => {"lorem" => "Hello OTA New"}}.to_yaml }
 
         before do
-          stub_request(:get, url_en).to_return(status: 200, body: body_en, headers: {})
-          stub_request(:get, url_en_2).to_return(status: 200, body: body_en_2, headers: {})
+          stub_request(:get, url_en).to_return(status: 200, body: body_en)
+          stub_request(:get, url_en_2).to_return(status: 200, body: body_en_2)
         end
 
         it do
@@ -69,6 +70,29 @@ describe Phrase::Ota::Backend do
 
           subject.send(:update_translations)
           expect(I18n.t(:lorem, locale: :en)).to eq("Hello OTA New")
+        end
+      end
+
+      context "store current_version per locale" do
+        let(:available_locales) { [:en, :fr] }
+        let(:body_fr) { {"fr" => {"lorem" => "Bonjour le monde"}}.to_yaml }
+        let(:url_fr) { "https://ota.eu.phrase.com/#{distribution_id}/#{secret_token}/fr/yml?app_version&client=ruby&sdk_version=#{Phrase::Ota::VERSION}" }
+        let(:url_en_2) { "https://ota.eu.phrase.com/#{distribution_id}/#{secret_token}/en/yml?app_version&client=ruby&current_version=0&sdk_version=#{Phrase::Ota::VERSION}" }
+
+        before do
+          stub_request(:get, url_en).to_return(status: 200, body: body_en)
+          stub_request(:get, url_en_2).to_return(status: 200, body: body_en)
+          stub_request(:get, url_fr).to_return({status: 404}, {status: 200, body: body_fr})
+        end
+
+        it do
+          subject.send(:update_translations)
+          expect(I18n.t(:lorem, locale: :en)).to eq("Hello OTA")
+          expect(I18n.t(:lorem, locale: :fr)).to eq("Translation missing: fr.lorem")
+
+          subject.send(:update_translations)
+          expect(I18n.t(:lorem, locale: :en)).to eq("Hello OTA")
+          expect(I18n.t(:lorem, locale: :fr)).to eq("Bonjour le monde")
         end
       end
     end
